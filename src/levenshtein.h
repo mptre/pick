f82a48624eb15056c31c51b5897ac7f84809b014
Gabor Buella <gbuella@gmail.com>
2015-06-27 22:55:41 +0200

    Implement edit distance based substring matching

--- src/levenshtein.h
+++ src/levenshtein.h
@@ -8,17 +8,9 @@
  * edit distance ( <= max_distance ) from a 
  * or max_distance -1, if no such substring is found
  */
-static inline unsigned levenshtein_substring_match(
+unsigned levenshtein_substring_match(
 	const char *a, unsigned length_a,
 	const char *b, unsigned length_b,
-	unsigned max_distance)
-{
-	(void)a;
-	(void)b;
-	(void)length_a;
-	(void)length_b;
-	(void)max_distance;
-	return 0;
-}
+	unsigned max_distance);
 
 #endif /* LEVENSHTEIN_H */
