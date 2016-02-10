da6b6c85ab81e532d29c5b401e11113f22214879
Anton Lindqvist <anton.lindqvist@gmail.com>
2016-02-10 17:30:38 +0100

    Don't allow the pick process to run forever

--- tests/test.sh
+++ tests/test.sh
@@ -25,7 +25,8 @@ main() {
   in=$(mktemp pick.XXXX)
   exp=$(mktemp pick.XXXX)
   act=$(mktemp pick.XXXX)
-  trap 'rm "$in" "$exp" "$act"' EXIT
+  err=$(mktemp pick.XXXX)
+  trap 'rm "$in" "$exp" "$act" "$err"' EXIT
 
   [ $# -eq 0 ] && set $(find "$DIR" -name '*.in')
   for a
@@ -33,11 +34,11 @@ main() {
     e=$(field exit <"$a" || echo 0)
 
     field output <"$a" >"$exp"
-    pick "$a" >"$act" 2>/dev/null
+    pick "$a" >"$act" 2>"$err"
     e=$((e ^ $?))
 
     [ $e -ne 0 ] && { echo "${a}: wrong exit code" 1>&2; exit 1; }
-    ! diff -c "$exp" "$act" && exit 1
+    ! diff -c "$exp" "$act" && { cat "$err"; exit 1; }
   done
   return 0
 }
