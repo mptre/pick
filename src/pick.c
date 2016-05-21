mptre/pick

commit 6de1fe1bcc1154a0f404d2769808b9b11f899ba7

Highlight the matched substring
By using the underline capability the matched substring for all choices
is highlighted. This change gives a better notion of the actual match
and allows one to easily spot matched outliers.

The printing of choices is rewritten which removes the need to allocate
a new string for every matched choice and usage of two compat functions.

src/pick.c
@@ -37,6 +37,7 @@
 #define EX_SIG 128
 #define EX_SIGINT (EX_SIG + SIGINT)
 
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define tty_putp(capability) do { \
 	if (tputs(capability, 1, tty_putc) == ERR) \
 		errx(1, #capability ": unknown terminfo capability"); \
@@ -45,6 +46,9 @@
 struct choice {
 	char	*description;
 	char	*string;
+	size_t	 length;
+	size_t	 match_start;
+	size_t	 match_end;
 	float	 score;
 };
 
@@ -55,14 +59,14 @@ static char			*eager_strpbrk(const char *, const char *);
 static void			 put_choice(const struct choice *);
 static const struct choice	*selected_choice(void);
 static void			 filter_choices(void);
-static float			 score(char *);
-static size_t			 min_match_length(char *);
+static void			 score(struct choice *);
+static int			 min_match(const char *, size_t, size_t *, size_t *);
 static char			*strcasechr(const char *, char);
 static void			 init_tty(void);
 static int			 tty_putc(int);
 static void			 handle_sigint(int);
 static void			 restore_tty(void);
-static void			 put_line(char *, int, int);
+static void			 put_line(char *, int);
 static int			 print_choices(int);
 static int			 get_key(void);
 static int			 tty_getc(void);
@@ -210,8 +214,11 @@ get_choices(void)
 		else
 			description = "";
 
+		choices.v[choices.length].length = stop - start;
 		choices.v[choices.length].string = start;
 		choices.v[choices.length].description = description;
+		choices.v[choices.length].match_start = 0;
+		choices.v[choices.length].match_end = 0;
 		choices.v[choices.length].score = 0;
 
 		start = stop + 1;
@@ -429,7 +436,7 @@ filter_choices(void)
 	int	i;
 
 	for (i = 0; i < (ssize_t)choices.length; ++i)
-		choices.v[i].score = score(choices.v[i].string);
+		score(&choices.v[i]);
 
 	qsort(choices.v, choices.length, sizeof(struct choice), choicecmp);
 }
@@ -447,38 +454,45 @@ choicecmp(const void *p1, const void *p2)
 	return c1->string - c2->string;
 }
 
-float
-score(char *string)
+void
+score(struct choice *choice)
 {
-	size_t	string_length, query_length, match_length;
+	size_t	query_length, match_length;
 
-	if ((match_length = min_match_length(string)) == INT_MAX)
-		return 0;
+	if (min_match(choice->string, 0,
+		      &choice->match_start, &choice->match_end) == 0) {
+		choice->match_start = choice->match_end = choice->score = 0;
+		return;
+	}
 
-	string_length = strlen(string);
+	match_length = choice->match_end - choice->match_start;
 	query_length = strlen(query);
-
-	return (float)query_length / match_length / string_length;
+	choice->score = (float)query_length / match_length / choice->length;
 }
 
-size_t
-min_match_length(char *string)
+int
+min_match(const char *string, size_t offset, size_t *start, size_t *end)
 {
-	char	*start, *end;
+	char	*s, *e;
 	int	 i;
-	size_t	 length, min;
 
-	if (!(start = strcasechr(string, query[0])))
-		return INT_MAX;
-	for (i = 1, end = start + 1; *end && query[i]; end++)
-		if (strncasecmp(end, query + i, 1) == 0)
+	if (!(s = strcasechr(string + offset, query[0])))
+		return 0;
+	for (i = 1, e = s + 1; *e && query[i]; e++)
+		if (strncasecmp(e, query + i, 1) == 0)
 			i++;
 	if (query[i])
-		return INT_MAX;
-	length = end - start;
-	min = min_match_length(string + 1);
+		return 0;
 
-	return length < min ? length : min;
+	*start = s - string;
+	*end = e - string;
+	/* Less than or equal is used in order to obtain the left-most match. */
+	if (min_match(string, offset + 1, start, end)
+	    && (size_t)(e - s) <= *end - *start) {
+		*start = s - string;
+		*end = e - string;
+	}
+	return 1;
 }
 
 char *
@@ -552,12 +566,9 @@ restore_tty(void)
 }
 
 void
-put_line(char *string, int length, int standout)
+put_line(char *string, int length)
 {
 	int	i;
-
-	if (standout)
-		tty_putp(enter_standout_mode);
 
 	for (i = 0; string[i] != '\0' && i < columns; ++i) {
 		if (tty_putc(string[i]) == EOF)
@@ -567,9 +578,6 @@ put_line(char *string, int length, int standout)
 		if (tty_putc(' ') == EOF)
 			err(1, "tty_putc");
 	}
-
-	if (standout)
-		tty_putp(exit_standout_mode);
 }
 
 void
@@ -578,7 +586,7 @@ print_query(char *query, size_t length, size_t position, size_t scroll)
 	size_t	i;
 
 	tty_putp(restore_cursor);
-	put_line(query + scroll, length - scroll, 0);
+	put_line(query + scroll, length - scroll);
 
 	tty_putp(restore_cursor);
 	for (i = 0; i < position - scroll; ++i)
@@ -588,16 +596,12 @@ print_query(char *query, size_t length, size_t position, size_t scroll)
 int
 print_choices(int selection)
 {
-	char		*line;
-	int		 i;
-	size_t		 length, line_length = 64, query_length;
+	int		 i, j;
+	size_t		 length, query_length;
 	struct choice	*choice;
 
 	/* Emit query line. */
 	tty_putc('\n');
-
-	if ((line = calloc(sizeof(*line), line_length)) == NULL)
-		err(1, "calloc");
 
 	query_length = strlen(query);
 	for (choice = choices.v, i = 0;
@@ -605,25 +609,42 @@ print_choices(int selection)
 	     && i < lines - 1
 	     && (query_length == 0 || choice->score > 0);
 	     choice++, i++) {
-		length = strlen(choice->string) + strlen(choice->description) +
-		    1;
+		if (i == selection)
+			tty_putp(enter_standout_mode);
 
-		while (length > line_length) {
-			line_length = line_length * 2;
+		for (j = 0, length = MIN(choice->match_start, (size_t)columns);
+		     j < (ssize_t)length;
+		     j++)
+			if (tty_putc(choice->string[j]) == EOF)
+				err(1, "tty_putc");
 
-			if ((line = reallocarray(line, line_length,
-					    sizeof(*line))) == NULL)
-				err(1, "reallocarray");
-		}
+		tty_putp(enter_underline_mode);
 
-		strlcpy(line, choice->string, line_length);
-		strlcat(line, " ", line_length);
-		strlcat(line, choice->description, line_length);
+		for (length = MIN(choice->match_end, (size_t)columns);
+		     j < (ssize_t)length;
+		     j++)
+			if (tty_putc(choice->string[j]) == EOF)
+				err(1, "tty_putc");
 
-		put_line(line, length, i == selection);
+		tty_putp(exit_underline_mode);
+
+		for (length = MIN(choice->length, (size_t)columns);
+		     j < (ssize_t)length;
+		     j++)
+			/* A null character will be present before the
+			 * terminating null character if descriptions is
+			 * enabled. */
+			if (tty_putc(choice->string[j] == '\0'
+				     ? ' ' : choice->string[j]) == EOF)
+				err(1, "tty_putc");
+
+		for (; j < columns; j++)
+			if (tty_putc(' ') == EOF)
+				err(1, "tty_putc");
+
+		if (i == selection)
+			tty_putp(exit_standout_mode);
 	}
-
-	free(line);
 
 	return i;
 }

config.h.in
@@ -47,12 +47,6 @@
 /* Define to 1 if you have the <string.h> header file. */
 #undef HAVE_STRING_H
 
-/* Define to 1 if you have the `strlcat' function. */
-#undef HAVE_STRLCAT
-
-/* Define to 1 if you have the `strlcpy' function. */
-#undef HAVE_STRLCPY
-
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #undef HAVE_SYS_STAT_H
 

configure.ac
@@ -8,7 +8,7 @@ AC_TYPE_SIZE_T
 AC_TYPE_SSIZE_T
 AC_FUNC_MALLOC
 AC_FUNC_REALLOC
-AC_CHECK_FUNCS([memmove reallocarray setlocale strdup strlcpy strlcat])
+AC_CHECK_FUNCS([memmove reallocarray setlocale strdup])
 AC_SEARCH_LIBS([initscr], [ncursesw ncurses], [], [
   AC_MSG_ERROR([unable to find initscr() function. Try to install ncurses])
 ])

src/compat.c
@@ -9,124 +9,6 @@
  */
 int unused;
 
-#ifndef HAVE_STRLCPY
-
-/*	$OpenBSD: strlcpy.c,v 1.11 2006/05/05 15:27:38 millert Exp $	*/
-
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-/* OPENBSD ORIGINAL: lib/libc/string/strlcpy.c */
-
-#include <sys/types.h>
-#include <string.h>
-
-/*
- * Copy src to string dst of size siz.  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
- */
-size_t
-strlcpy(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-
-	/* Copy as many bytes as will fit */
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
-
-	return(s - src - 1);	/* count does not include NUL */
-}
-
-#endif /* !HAVE_STRLCPY */
-
-#ifndef HAVE_STRLCAT
-
-/*	$OpenBSD: strlcat.c,v 1.13 2005/08/08 08:05:37 espie Exp $	*/
-
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-#include <sys/types.h>
-#include <string.h>
-
-/*
- * Appends src to string dst of size siz (unlike strncat, siz is the
- * full size of dst, not space left).  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
- * Returns strlen(src) + MIN(siz, strlen(initial dst)).
- * If retval >= siz, truncation occurred.
- */
-size_t
-strlcat(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-	size_t dlen;
-
-	/* Find the end of dst and adjust bytes left but don't go past end */
-	while (n-- != 0 && *d != '\0')
-		d++;
-	dlen = d - dst;
-	n = siz - dlen;
-
-	if (n == 0)
-		return(dlen + strlen(s));
-	while (*s != '\0') {
-		if (n != 1) {
-			*d++ = *s;
-			n--;
-		}
-		s++;
-	}
-	*d = '\0';
-
-	return(dlen + (s - src));	/* count does not include NUL */
-}
-
-#endif /* !HAVE_STRLCAT */
-
 #ifndef HAVE_REALLOCARRAY
 
 /*	$OpenBSD: reallocarray.c,v 1.2 2014/12/08 03:45:00 bcook Exp $	*/

src/compat.h
@@ -17,22 +17,6 @@
 #endif
 #endif /* __linux__ || __CYGWIN__ */
 
-#ifndef HAVE_STRLCPY
-
-#include <sys/types.h>
-
-size_t	strlcpy(char *, const char *, size_t);
-
-#endif /* !HAVE_STRLCPY */
-
-#ifndef HAVE_STRLCAT
-
-#include <sys/types.h>
-
-size_t	strlcat(char *, const char *, size_t);
-
-#endif /* !HAVE_STRLCAT */
-
 #ifndef HAVE_REALLOCARRAY
 
 void	*reallocarray(void *, size_t, size_t);

commit bc7e6cefc3b481cc36ec1a9e307d87874bd5e2f3

Remove limits include
Regression from 6de1fe1.

src/pick.c
@@ -5,7 +5,6 @@
 #include <sys/ttydefaults.h>
 #include <ctype.h>
 #include <err.h>
-#include <limits.h>
 #include <locale.h>
 #include <signal.h>
 #include <stdio.h>