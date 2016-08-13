mptre/pick

commit 1ba7398ff6846c65045114b8c41fff4cdaa9789f

Improve search performance by not wasting cycles
Using pick with a large number of choices is slow. The threshold on my
computer is somewhere around 10k lines of input. Benchmarking the
filtering revealed that the scoring is the bottleneck. This is no
surprise since the current search algorithm is an O(n^2) operation in
worst case. The second most time consuming task is the sorting of the
choices. However, the time taken for qsort(3) is consistent and several
order of magnitude less time consuming than the scoring.

When the user inputs a query longer greater than 1 character we
currently waste lots of cycles by performing a search every time a new
character is received. Even if the current query is outdated since the
user already has typed more character yet to be processed. Therefore,
abort the search if there is unprocessed user input available. This is
done by performing a non-blocking poll(2) call on the user input file
descriptor. The check for new input is done on every 50:th iteration.
The number is empirically chosen and close to the number of lines in my
terminal. There's no guarantee that this number is optimal for everyone.
Experimenting with the number on different computers is therefore of
interest.

This change gives the notion of a responsive interface even when the
input is large. Running the following command prior and after this
change should result in a noticeable speed-up:

  $ find ~ | pick

I think this a decent trade-off in terms of gained performance vs.
complexity.

While at it, remove a cast by changing the type of the local i variable
and replace pre-increment with post-increment.

src/pick.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <err.h>
 #include <locale.h>
+#include <poll.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -464,13 +465,35 @@ selected_choice(void)
 	}
 }
 
+/*
+ * Filter choices using the current query while there is no new user input
+ * available.
+ */
 void
 filter_choices(void)
 {
-	int	i;
+	struct pollfd	pfd;
+	size_t		i;
+	int		nready;
 
-	for (i = 0; i < (ssize_t)choices.length; ++i)
+	for (i = 0; i < choices.length; i++) {
 		score(&choices.v[i]);
+
+		/*
+		 * Regularly check if there is any new user input available. If
+		 * true, abort filtering since the currently used query is
+		 * outdated. This improves the performance when the cardinality
+		 * of the choices is large.
+		 */
+		if (i % 50 == 0) {
+			pfd.fd = fileno(tty_in);
+			pfd.events = POLLIN;
+			if ((nready = poll(&pfd, 1, 0)) == -1)
+				err(1, "poll");
+			if (nready == 1 && pfd.revents & (POLLIN | POLLHUP))
+				break;
+		}
+	}
 
 	qsort(choices.v, choices.length, sizeof(struct choice), choicecmp);
 }

commit ffc0ef20f23443ed5318a61f1e17c0d8324bcdb6

Add query option test and fix timing issue
The test spotted a bug related to filter abort introduced in 1ba7398.
The pick-test command enters keyboard input at a very high pace causing
the filtering to be aborted since the input file descriptor contains
unexamined data and 0 mod any number will always equal 0. Therefore,
only abort if at least 50 choices has already been filtered. This is not
a exhaustive solution, consider the following scenario: the number of
choices is greater than 50 and the search is slow, the user inputs any
key that won't affect the query (like choice movement) the filtering
will then be aborted leaving the filtered choices in a bad state. The
correct solution would be to peek at the input file descriptor and only
abort if the first byte is a printable ASCII or UTF-8 character. Such a
solution is complex and this scenario is unlikely to occur.

Makefile.am
@@ -12,7 +12,7 @@ TESTS=tests/01.t tests/02.t tests/03.t tests/04.t tests/05.t tests/06.t \
       tests/13.t tests/14.t tests/15.t tests/16.t tests/17.t tests/18.t \
       tests/19.t tests/20.t tests/21.t tests/22.t tests/23.t tests/24.t \
       tests/25.t tests/26.t tests/27.t tests/28.t tests/29.t tests/30.t \
-      tests/31.t tests/32.t tests/33.t tests/34.t
+      tests/31.t tests/32.t tests/33.t tests/34.t tests/35.t
 TEST_EXTENSIONS=.t
 T_LOG_COMPILER=sh tests/test.sh
 check_PROGRAMS=tests/pick-test

src/pick.c
@@ -492,7 +492,7 @@ filter_choices(void)
 		 * outdated. This improves the performance when the cardinality
 		 * of the choices is large.
 		 */
-		if (i % 50 == 0) {
+		if (i > 0 && i % 50 == 0) {
 			pfd.fd = fileno(tty_in);
 			pfd.events = POLLIN;
 			if ((nready = poll(&pfd, 1, 0)) == -1)

tests/35.t
@@ -1,0 +1,8 @@
+description: filter using the query option
+args: -q a
+keys: \\n
+stdin:
+b
+a
+stdout:
+a