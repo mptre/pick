7f5694ac1cf3a68b462a9f22e03a12a719249ee5
Anton Lindqvist <anton.lindqvist@gmail.com>
2016-12-27 10:26:19 +0100

    Only define __dead if it's missing

--- compat.h
+++ compat.h
@@ -5,19 +5,9 @@
 #include "config.h"
 #endif
 
-#ifdef __FreeBSD__
-#define __dead __dead2
-#endif /* __FreeBSD__ */
-
-#if defined(__linux__) || defined(__CYGWIN__)
 #ifndef __dead
-#ifdef __GNUC__
-#define __dead		__attribute__((__noreturn__))
-#else
 #define __dead
 #endif
-#endif
-#endif /* __linux__ || __CYGWIN__ */
 
 #ifndef HAVE_REALLOCARRAY
 
