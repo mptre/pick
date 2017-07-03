bf666ed43d3e13c4234487585589f82370608d8e
Anton Lindqvist <anton.lindqvist@gmail.com>
2017-07-03 23:08:54 +0200

    Revert "Only define __dead if it's missing"
    This reverts commit 7f5694ac1cf3a68b462a9f22e03a12a719249ee5.
    
    Partial fix to #202.

--- compat.h
+++ compat.h
@@ -5,9 +5,19 @@
 #include "config.h"
 #endif
 
+#ifdef __FreeBSD__
+#define __dead __dead2
+#endif /* __FreeBSD__ */
+
+#if defined(__linux__) || defined(__CYGWIN__)
 #ifndef __dead
+#ifdef __GNUC__
+#define __dead		__attribute__((__noreturn__))
+#else
 #define __dead
 #endif
+#endif
+#endif /* __linux__ || __CYGWIN__ */
 
 #ifndef HAVE_REALLOCARRAY
 
